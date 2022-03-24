815920a2515f24b94178c17af4c4b86312c5342a
Eugene <marking.eugene@gmail.com>
2022-03-24 09:06:16 -0400

    Autodeploy script FIX

--- autodepoy.sh
+++ autodepoy.sh
@@ -5,7 +5,8 @@
 
 echo "Trying to yc init......"
 
-yc compute instance create --name reddit-app --hostname reddit-app  --zone ru-central1-a
---create-boot-disk image-folder-id=standard-images,image-family=ubuntu-1804,size=10GB
---platform standard-v3 --cores=2 --core-fraction=20 --memory=4 --preemptible
---network-interface subnet-name=mynet,nat-ip-version=ipv4, --metadata serial-port-enable=1--metadata-from-file user-data=setting.yaml
+yc compute instance create --name reddit-app  --hostname  reddit-app --zone ru-central1-a \
+ --create-boot-disk image-folder-id=standard-images,image-family=ubuntu-2004-lts,size=10GB \
+ --platform standard-v3  --cores=2  --core-fraction=20  --memory=4 --preemptible \
+ --network-interface subnet-name=mynet,nat-ip-version=ipv4 \
+ --metadata serial-port-enable=1  --metadata-from-file user-data=setting.yaml
