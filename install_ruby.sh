26d1594f6484ce85f6c20822e9738688d2cc695e
Eugene <marking.eugene@gmail.com>
2022-03-25 04:12:55 -0400

    HW 6 Delete scrips ruby

--- install_ruby.sh
+++ install_ruby.sh
@@ -7,4 +7,4 @@ echo "Starting to install Ruby env....."
 
 sudo apt-get --assume-yes update
 
-sudo ap-get install -y ruby-full ruby-bundler build-essential
+sudo apt-get --assume-yes install ruby-full ruby-bundler build-essential
