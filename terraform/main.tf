b05bcad115c81b5aeccc058899715eb3479a6242
Eugene <marking.eugene@gmail.com>
2022-04-12 02:17:51 -0400

    Lesson 7 HW

---
+++ terraform/main.tf
@@ -0,0 +1,63 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+}
+
+provider "yandex" {
+  service_account_key_file = var.service_acc_key
+  cloud_id                 = var.cloud_id
+  folder_id                = var.folder_id
+  zone                     = var.zone_id
+}
+
+resource "yandex_compute_instance" "app" {
+  name        = "reddit-app-${count.index}"
+  platform_id = "standard-v3"
+  count       = var.inst_count
+
+  resources {
+    cores         = 2
+    memory        = 4
+    core_fraction = 20
+
+  }
+  scheduling_policy {
+    preemptible = true
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = var.image_id
+    }
+  }
+  network_interface {
+    nat       = true
+    subnet_id = var.subnet_id
+
+  }
+
+  provisioner "file" {
+    source      = "files/puma.service"
+    destination = "/tmp/puma.service"
+
+  }
+  provisioner "remote-exec" {
+    script = "files/deploy.sh"
+
+  }
+  connection {
+    type        = "ssh"
+    host        = self.network_interface.0.nat_ip_address
+    agent       = false
+    user        = "ubuntu"
+    private_key = file("~/.ssh/admin")
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file(var.public_key_path)}"
+  }
+
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,30 @@
+variable "cloud_id" {
+  default = "b1g8fd7t7ngogsno9or1"
+}
+
+variable "folder_id" {
+  default = "b1gjs8uoo12oe6e1hh51"
+}
+
+variable "zone_id" {
+  default = "ru-central1-a"
+}
+
+variable "public_key_path" {
+  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCYkwj+2bTUEqYk/QMfsyPa2eDaljmzBeM33tWXKAlGexTUurzTg2Kp4xUj+cIZI4gK7bDdWQLXGThT8fWS9e3oxvJrZ254YodA4WaGjZXPwXKKz+8shd+gJBVDgrG5ucLHlEWDeGzxx49MWBvN7/fixyxjQ6PBhKhzKR4XTnkgdxSxar2+0tDSyZq81rR7Msl5OaHwhyDXVp+alAXyaZoyoz2BYn9H1xyH1HY77IKeL411Ltd2mCO1aN+PChwZraJupimMaUUdIy6aL0j0mBhCuY7hEtJusCupcppe4/HfvtwVaONfPeBv0jp5gFIKp4Ojv0HYlqomtqLnHlWR4RUt6ExNDswOMCzopt+HHqlL8YZ/+p5kihH7jBiqhmG5ePy0HUF1ZBhCrYYu/+m4STdIPB+laUtYMCKA5bX8gGPo+spoRY/Gq0sEdB7WwZp3vYuTyxf/Di0FMEgGwVgZ+hVLQKo8FTYgzM8NpOeazpgKXVc6iBYQQZevcXjzbpaODbVguAzbDzshXHLQ2I/1Eu1YcGFKoLkKTtIgAC6aD3Hushrm4NA6mr67iJozavTQKNM3NYv2JWJgSrOjVv9H8AYVyP3S0riR6+bMV6i2st/ksNkekEF0/Exrsu3FykZkVkm3QMlpPv8dncgZ58ejPhflTGCp2akKN76Q2J8IF8bXbQ== admin"
+
+}
+variable "service_acc_key" {
+  default = "key.json"
+}
+variable "image_id" {
+  default = "fd8oino4d914g766eij1"
+}
+
+variable "subnet_id" {
+  default = "e9bq2oe6jsaapniab331"
+}
+
+variable "inst_count" {
+  default = "1"
+}
