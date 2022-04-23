01c4a2e35a245e787f3a512d4d42236431a1ff67
Eugene <marking.eugene@gmail.com>
2022-04-22 20:31:57 -0400

    Ansible-1 HW

--- terraform/modules/db/main.tf
+++ terraform/modules/db/main.tf
@@ -46,7 +46,7 @@ resource "null_resource" "db" {
     host        = var.host
     agent       = false
     user        = "ubuntu"
-    private_key = file("~/.ssh/appuser")
+    private_key = ${file(var.public_key_path)}
   }
 
   provisioner "remote-exec" {
