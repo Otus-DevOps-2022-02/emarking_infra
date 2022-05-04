bf4f825f77f7e6a014db64cb5b45b11b1f17fa64
Eugene Marking <marking.eugene@gmail.com>
2022-05-05 00:41:52 +0500

    Ansible-3

--- terraform/modules/app/main.tf
+++ terraform/modules/app/main.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+    local = {
+      source = "hashicorp/local"
+    }
+  }
+}
+
+
+
 resource "yandex_compute_instance" "app" {
   name        = "reddit-app-${count.index}-${var.env_type}"
   platform_id = "standard-v3"
@@ -30,37 +43,101 @@ resource "yandex_compute_instance" "app" {
     ssh-keys = "ubuntu:${file(var.public_key_path)}"
   }
 
+
 }
-resource "null_resource" "app" {
-  count = var.run_prv ? 1 : 0
+
+#Provisioning
+# resource "null_resource" "app" {
+#   #   #count = var.run_prv ? 1 : 0
+
+#   count = var.inst_count
+#   connection {
+#     type        = "ssh"
+#     host        = yandex_compute_instance.app[count.index].network_interface.0.nat_ip_address
+#     agent       = false
+#     user        = "ubuntu"
+#     private_key = file("/home/eugene/.ssh/admin")
+#   }
+
+#   #   provisioner "file" {
+#   #     source      = "/home/teraf1/modules/app/files/puma.service"
+#   #     destination = "/tmp/puma.service"
+
+#   #   }
+#   provisioner "remote-exec" {
+#     script = "../modules/app/files/deploy.sh"
+
+#   }
+
+#   # provisioner "remote-exec" {
+#   #   inline = [
+#   #     #       "sudo sed -i 's/Environment=/Environment=DATABASE_URL= ${element(tolist("${var.db_ip}"), count.index)}:27017/g' /tmp/puma.service",
+#   #     #       #"sudo sed -i 's/Environment=/Environment=DATABASE_URL=${element("${module.db[*].external_ip_address_db[0]}", count.index)}:27017/g' /tmp/puma.service",
+#   #     #       #"sudo sed -i 's/Environment=/Environment=DATABASE_URL=${module.db[count.index].external_ip_address_db[0]}:27017/g' /tmp/puma.service",
+#   #     #       "sudo mv /tmp/puma.service /etc/systemd/system/puma.service",
+#   #     "sudo systemctl daemon-reload",
+#   #     "sudo systemctl start puma",
+#   #     "sudo systemctl enable puma"
+#   #     #       #"echo ${join(" ", element("${var.db_ip}", count.index))})"
 
 
-  connection {
-    type        = "ssh"
-    host        = var.host
-    agent       = false
-    user        = "ubuntu"
-    private_key = var.public_key_path
+#   #   ]
+
+#   # }
+#   # depends_on = [
+#   #   yandex_compute_instance.app
+#   # ]
+# }
+
+locals {
+
+  instance_ips_app_st = yandex_compute_instance.app[*].network_interface.0.nat_ip_address
+  instance_na_app_st  = yandex_compute_instance.app[*].name
+  instance_env_type   = var.env_type
+}
+
+resource "local_file" "get_inst_data_stage" {
+
+  content = templatefile("./instapp.tpl", {
+
+    instance_names_app_st = local.instance_na_app_st,
+    ips_app_st            = local.instance_ips_app_st,
+    app_name              = "reddit-"
+    envt                  = var.env_type
+
+
+  })
+
+  filename = "../${var.env_type}/inst_app_${var.env_type}"
+
+
+
+
+}
+
+resource "null_resource" "templates" {
+
+  triggers = {
+
+    env_type = var.env_type
   }
 
-  provisioner "file" {
-    source      = "../modules/app/files/puma.service"
-    destination = "/tmp/puma.service"
+
+  provisioner "local-exec" {
+
+    when       = destroy
+    command    = "mv ../${self.triggers.env_type}/inst_app_${self.triggers.env_type} ../${self.triggers.env_type}/inst_app_${self.triggers.env_type}.back | mv ../${self.triggers.env_type}/inst_all_${self.triggers.env_type} ../${self.triggers.env_type}/inst_all_${self.triggers.env_type}.back"
+    on_failure = continue
 
   }
-  provisioner "remote-exec" {
-    script = "../modules/app/files/deploy.sh"
+  provisioner "local-exec" {
+    when = create
+
+    command = "touch ../${self.triggers.env_type}/inst_all_${self.triggers.env_type} | cat ../${self.triggers.env_type}/inst_app_${self.triggers.env_type} | tee -a ../${self.triggers.env_type}/inst_all_${self.triggers.env_type}"
+
+    on_failure = continue
 
   }
-  provisioner "remote-exec" {
-    inline = [
-      "sudo sed 's/Environment=/Environment=DATABASE_URL=${var.db_ip}:27017/g' /etc/systemd/system/puma.service | sudo tee /etc/systemd/system/puma.service",
-      "sleep 10",
-      "sudo systemctl daemon-reload",
-      "sudo systemctl restart puma"
 
-    ]
-
-  }
 
 }
